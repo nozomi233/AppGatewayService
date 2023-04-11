37fcf938fa476e3052ca98ba099f53ef46f201cc
祝浪 <zhulang013@hellobike.com>
2023-04-11 15:22:59 +0800

    rmi

---
+++ src/main/java/com/zhulang/rmi/RMIClient.java
@@ -0,0 +1,31 @@
+package com.zhulang.rmi;
+
+import com.zhulang.dto.User;
+import com.zhulang.iface.IUserService;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+/**
+ * @Author zhulang
+ * @Date 2023-04-11
+ **/
+public class RMIClient {
+    public static void main(String[] args) throws RemoteException {
+        // 1 获取 registry 实例
+        Registry registry = LocateRegistry.getRegistry("172.17.170.30",29999);
+        // 2 通过 registry 实例查找对应的远程对象
+//        for (String name : registry.list()) {
+//            System.out.println(name);
+//        }
+        try {
+            IUserService userService = (IUserService) registry.lookup("userService");
+            User user = userService.findById(1);
+            System.out.println(user.getId()+"-----"+ user.getName());
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
